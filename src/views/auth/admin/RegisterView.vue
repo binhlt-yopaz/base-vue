d09674caa8ea9b1a71fc9c0152168e460464d63b
nguyenquanghuy61ht <huyn67811@gmail.com>
2023-09-29 10:00:27 +0700

    add input confirm password

--- src/views/auth/admin/RegisterView.vue
+++ src/views/auth/admin/RegisterView.vue
@@ -22,7 +22,17 @@
             :regiter="password"
           />
         </div>
-        <button class="mt-3 px-2 py-1 bg-blue-500 text-white">Register</button>
+        <div class="flex flex-col">
+          <label for="confirm_password">Confirm password</label>
+          <InputField
+            type="password"
+            name="confirm_password"
+            inputClass="p-2 my-1"
+            :errors="errors"
+            :regiter="confirm_password"
+          />
+        </div>
+        <button class="mt-3 px-2 py-1 bg-blue-500 text-white transition-transform duration-300 ease-in-out">Register</button>
       </Form>
     </el-card>
   </div>
@@ -37,6 +47,9 @@ import * as yup from 'yup';
 const schema = yup.object({
   email: yup.string().required('email required').email('Email invalid'),
   password: yup.string().required('Password required'),
+  confirm_password: yup.string()
+    .required()
+    .oneOf([yup.ref('password')], 'Passwords do not match'),
 });
 
 const { errors, defineInputBinds, handleSubmit } = useForm({
@@ -45,6 +58,7 @@ const { errors, defineInputBinds, handleSubmit } = useForm({
 
 const email = defineInputBinds('email');
 const password = defineInputBinds('password');
+const confirm_password = defineInputBinds('confirm_password');
 
 const onSubmit = handleSubmit(async(values) => {
   const res = await store.dispatch('authStore/register', values);
